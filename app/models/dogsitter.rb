89765c92ec862871fabe640bdc863cfbeba8278e
Ecris ton nom ici, en gardant les quotes <Ecris ton email ici, en gardant les quotes>
2021-07-30 09:25:28 +0200

    correction des relations dans le model. nettoyage du seed

--- app/models/dogsitter.rb
+++ app/models/dogsitter.rb
@@ -1,5 +1,5 @@
 class Dogsitter < ApplicationRecord
     has_many :strolls, dependent: :destroy
     has_many :dogs, through: :strolls
-
+    belongs_to :city
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,31 @@
 
 # Stroll.create(date:'2021-07-21',dog_id:1,dogsitter_id:1)
 
+City.destroy_all
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'cities'")
+Dog.destroy_all
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'dogs'")
+Dogsitter.destroy_all
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'dogsitters'")
+Stroll.destroy_all
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'strolls'")
 
-# city_zip = [["Paris","75000"],["Lyon","69000"],["Marseille","13000"],["Nice","06000"],["Bordeaux","33000"],["Strasbourg","67000"],["Rennes","35000"],["Toulouse","31000"],["Caen","14000"],"Toulon","83000"]
 
-# 9.times do |i|
-#     City.create(city_name: (city_zip[i][0]), zip_code: (city_zip[i][1]))
-# end
+city_zip = [["Paris","75000"],["Lyon","69000"],["Marseille","13000"],["Nice","06000"],["Bordeaux","33000"],["Strasbourg","67000"],["Rennes","35000"],["Toulouse","31000"],["Caen","14000"],["Toulon","83000"]]
 
-20.times do |i|
-    # Dog.create(name:Faker::Name.name, age:rand(1..10), city_id:rand(1..City.count))
-    Dog.find(i+2).update(name:Faker::Name.first_name)
+10.times do |i|
+  City.create(city_name: (city_zip[i][0]), zip_code: (city_zip[i][1]))
+end
+
+
+10.times do
+  Dogsitter.create(name:Faker::Name.name, city_id:rand(1..City.count))
+end
+
+10.times do
+  Dog.create(name:Faker::Name.name.split[-1], age:rand(1..10), city_id:rand(1..City.count))
+end
+
+10.times do
+  Stroll.create(date:Faker::Time.between(from: "2014-09-19", to: DateTime.now), dog_id:rand(1..Dog.count), dogsitter_id:rand(1..Dogsitter.count), city_id:rand(1..City.count))
 end
